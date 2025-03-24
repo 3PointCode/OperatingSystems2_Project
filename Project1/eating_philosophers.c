9fec1cea6d70a3dab8eb1bc5c63a6f6bdfc5b516
Tomasz <tomasz.piwoda@gmail.com>
2025-03-24 19:13:43 +0100

    Added print_state function that is responsible for printing the current state of a philosopher. Defined three states in which every philosopher can be.

---
+++ Project1/eating_philosophers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+// Three states in which philosophers can be
+#define THINKING 0
+#define HUNGRY 1
+#define EATING 2
+
+// Print_state function is responsible for printing the current state of a philosopher
+void print_state(int philosopher, int state) {
+    if (state == THINKING) {
+        printf("Philosopher %d is thinking\n", philosopher);
+    } else if (state == HUNGRY) {
+        printf("Philosopher %d is hungry\n", philosopher);
+    } else {
+        printf("Philosopher %d is eating\n");
+    }
+}
